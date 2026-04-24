dd9899e596f203b5fcc0967bad4957d1f639e150
zhenya <zenamatusev68@gmail.com>
2026-04-24 20:46:36 +0300

    Add acquiring result handling: created AcquiringResult record and PaymentAcquiringClient interface, implemented RandomOrgAcquiringAdapter for external API integration with resilience and fallback handling.

---
+++ src/main/java/com/innowise/paymentservice/client/AcquiringResult.java
@@ -0,0 +1,5 @@
+package com.innowise.paymentservice.client;
+
+public record AcquiringResult(
+    int responseCode
+) {}
---
+++ src/main/java/com/innowise/paymentservice/client/impl/RandomOrgAcquiringAdapter.java
@@ -0,0 +1,46 @@
+package com.innowise.paymentservice.client.impl;
+
+import com.innowise.paymentservice.client.AcquiringResult;
+import com.innowise.paymentservice.client.PaymentAcquiringClient;
+import com.innowise.paymentservice.exception.external.RandomOrgUnavailableException;
+
+import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import lombok.RequiredArgsConstructor;
+import io.github.resilience4j.retry.annotation.Retry;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class RandomOrgAcquiringAdapter implements PaymentAcquiringClient {
+
+    @Qualifier("randomOrgWebClient")
+    private final WebClient randomOrgWebClient;
+
+    @Override
+    @Retry(name = "randomOrgAcquiring", fallbackMethod = "fallbackTransaction")
+    public AcquiringResult getAcquiringResult() {
+        String responseBody = randomOrgWebClient.get()
+                .uri("/random/integer")
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+
+        if (responseBody == null || responseBody.isBlank()) {
+            throw new RandomOrgUnavailableException("Empty response from Random.org");
+        }
+
+        int resultNumber = Integer.parseInt(responseBody.trim());
+
+        return new AcquiringResult(resultNumber);
+    }
+
+    public AcquiringResult fallbackTransaction(Exception e) {
+        log.error("Payment gateway is unavailable after all attempts! Reason: {}", e.getMessage());
+        throw new RandomOrgUnavailableException("Failed to connect to Random.org", e);
+    }
+
+}
